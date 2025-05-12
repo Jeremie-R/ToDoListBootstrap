39ca12e55baa704d73fd28c5a021ff8abc271ab0
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2025-05-12 13:45:44 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+
+
+
+function App() {
+
+    return (
+      <div>
+        <h2>potato</h2>
+      </div>
+    )
+  }
+  
+  export default App
