20e6ad8dc14dd315fbdef60884d0aa297f605d61
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2025-05-12 13:51:51 +0200

    Added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client"
 
 
 
-
-
 function App() {
 
     return (
       <div>
-        <h2>potato</h2>
+        <h2 className="text-center border-bottom">potato</h2>
       </div>
     )
   }
