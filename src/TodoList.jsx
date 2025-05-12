43b10e026fe30a5cb61754cdf864e786ce227357
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2025-05-12 14:34:51 +0200

    added basic structure no ux yet

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
+
 export default function TodoList() {
+
+    // create a state
+    const [todos, setTodos] = useState([
+        {task: "my first task", completed: false}, 
+        {task: "my second task", completed: true}])
+
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        let task = event.target.task.value;
+        alert("new task is: " + task)
+
+        event.target.reset()
+    }
+
     return (
         <div className="container my-5">
             <div className="mx-auto rounded border p-4" 
                 style={{width: "600px", backgroundColor: "#ECEFF2"}}>
+
                     <h2 className="text-black text-center mb-5">Simple todo list</h2>
+
+                    <form className="d-flex mb-2" onSubmit={handleSubmit}>
+                        <input className="form-control me-2" placeholder="write a new to do" name="task" />
+                        <button className="btn btn-outline-primary" type="submit">Add</button>
+                    </form>
+
+                    {
+                        todos.map((todo, index) => {
+                            return (
+                                <div key={index} 
+                                    className="rounded mt-a p-2 mb-2 d-flex" 
+                                    style={{backgroundColor: todo.completed ? "#C3DFCD" : "white"}}>
+                                        <div className="me-auto">
+                                            {todo.task}
+                                        </div>
+
+                                </div>
+                            )
+                        })
+                    }
 
             </div>
         </div>
